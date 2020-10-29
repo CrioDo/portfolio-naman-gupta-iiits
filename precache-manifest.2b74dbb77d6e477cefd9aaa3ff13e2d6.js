694082e2e613a01aa2e7dadf3d5a1dd9d3c018d8
CrioDo <rahul@criodo.com>
2020-10-29 20:52:21 +0530

    Updates

---
+++ precache-manifest.2b74dbb77d6e477cefd9aaa3ff13e2d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf651936e9700549c56964d654c59b8c",
+    "url": "/portfolio-naman-gupta-iiits/index.html"
+  },
+  {
+    "revision": "29dd4356583d593bf1d7",
+    "url": "/portfolio-naman-gupta-iiits/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "5c034b15e4c14aa2fba7",
+    "url": "/portfolio-naman-gupta-iiits/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "29dd4356583d593bf1d7",
+    "url": "/portfolio-naman-gupta-iiits/static/js/2.92ab5d80.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-naman-gupta-iiits/static/js/2.92ab5d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c034b15e4c14aa2fba7",
+    "url": "/portfolio-naman-gupta-iiits/static/js/main.ae2520a7.chunk.js"
+  },
+  {
+    "revision": "921a3ce2600060d0bc44",
+    "url": "/portfolio-naman-gupta-iiits/static/js/runtime-main.a22f726f.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-naman-gupta-iiits/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-naman-gupta-iiits/static/media/crio-logo.63be71d0.svg"
+  }
+]);
